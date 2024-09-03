3ae18c510b5292d153725d4342778cb7bfc8ef73
Lemintex <aaron_nobile@hotmail.co.uk>
2024-09-03 21:48:40 +0100

    Fixed bug with down

--- lua/visual-move/init.lua
+++ lua/visual-move/init.lua
@@ -1,15 +1,26 @@
 local M = {}
 
+M._get_line_number = function()
+	local row, _ = vim.api.nvim_win_get_cursor(0)
+	local len = vim.api.nvim_buf_line_count(0)
+	return row, len
+end
+
 vim.keymap.set("n", "<M-j>", function()
-	return M.down()
+	local num, len = M._get_line_number()
+	return M.down(table.unpack(num), len)
 end)
 
 vim.keymap.set("n", "<M-k>", function()
 	return M.up()
 end)
 
-M.down = function()
-	vim.api.nvim_feedkeys('"0ddj"0P', "n", {})
+M.down = function(num, len)
+	if num < len - 1 then
+		vim.api.nvim_feedkeys('"0ddj"0P', "n", {})
+	elseif num == len - 1 then -- handles a literal edge case
+		vim.api.nvim_feedkeys('"0dd"0p', "n", {})
+	end
 end
 
 M.up = function()
@@ -17,9 +28,3 @@ M.up = function()
 end
 
 return M
-
---1111111111111111
---  2222222222222222222222
---  3333333333333333
---  44444444444
---  5555555555555555
